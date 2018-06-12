a20846b4dc59ca6d28ffc4ad794054ecc2032b5a
Paris Kasidiaris <paris@sourcelair.com>
2018-06-12 14:09:21 +0300

    renames

--- public/main.js
+++ public/main.js
@@ -5,10 +5,12 @@ class SplitLayout {
         this.gutter = this.element.querySelector('.split-pane-gutter');
         console.debug('init split layout gutter', this.gutter);
         this.gutter.draggable = true;
+
         this.gutter.addEventListener('drag', e => {
             const rect = e.target.getBoundingClientRect();
             const parentRect = e.target.parentElement.getBoundingClientRect();
-            const pos = rect.x - parentRect.x - (rect.width / 2) + e.offsetX;
+            const base = rect.x - parentRect.x - (rect.width / 2);
+            const pos = base + e.offsetX;
             const gridTemplateColumns = `${pos}px 5px 5px auto`;
             this.element.style.gridTemplateColumns = gridTemplateColumns;
         });
