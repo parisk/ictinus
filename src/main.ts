b596e2279d0baf0c2d808fb289d85e402fea6827
Paris Kasidiaris <paris@sourcelair.com>
2018-06-14 15:10:45 +0300

    Horizontal and vertical work

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,31 @@
-class SplitLayout {
+export class SplitLayout {
+  public readonly elementA: HTMLElement;
   public readonly gutter: HTMLElement;
+  public readonly elementB: HTMLElement;
+  private readonly dimension: 'width' | 'height';
+  private readonly axis: 'clientX' | 'clientY';
+  private readonly rectAxis: 'left' | 'top';
+  private max: number;
   private isResizing: boolean = false;
   private rect: DOMRect;
 
-  constructor(public readonly container: HTMLElement) {
-    if (!container.classList.contains('split-pane-container')) {
-      throw new Error('Cannot initialize Split Layout in a container without the "split-pane-container" class.');
-    }
+  constructor(
+    public readonly container: HTMLElement,
+    public readonly orientation: 'horizontal' | 'vertical',
+    public readonly min: number = 100)
+  {
+    this.dimension = (this.orientation == 'horizontal') ? 'width' : 'height';
+    this.axis = (this.orientation == 'horizontal') ? 'clientX' : 'clientY';
+    this.rectAxis = (this.orientation == 'horizontal') ? 'left' : 'top';
+
+    this.container.classList.add('split-pane-container', `split-${this.orientation}`);
     this.gutter = this.container.querySelector('.split-pane-gutter');
 
     this.gutter.addEventListener('mousedown', e => {
       e.stopPropagation();
       this.isResizing = true;
       this.rect = <DOMRect>this.container.getBoundingClientRect();
+      this.max = this.rect[this.dimension] - this.min;
       this.container.addEventListener('mousemove', this.resize);
     });
 
@@ -26,19 +39,20 @@ class SplitLayout {
   }
 
   private resize = e => {
-    const min = 100;
-    const max = 700;
-    const pos = e.clientX - this.rect.left;
+    const pos = e[this.axis] - this.rect[this.rectAxis];
 
-    if (pos < min || pos > max) {
+    if (pos < this.min || pos > this.max) {
       return;
     }
 
-    const gridTemplateColumns = `${pos}px 5px 5px auto`;
-    this.container.style.gridTemplateColumns = gridTemplateColumns;
+    const ratio = pos / this.rect[this.dimension] * 100;
+
+    if (this.orientation == 'horizontal') {
+      const gridTemplateColumns = `${ratio.toFixed(3)}% 5px 5px auto`;
+      this.container.style.gridTemplateColumns = gridTemplateColumns;
+    } else {
+      const gridTemplateRows = `${ratio.toFixed(3)}% 5px 5px auto`;
+      this.container.style.gridTemplateRows = gridTemplateRows;
+    }
   }
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-  new SplitLayout(document.querySelector('#le-layout'));
-});
