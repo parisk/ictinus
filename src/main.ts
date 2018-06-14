88972157d5c2797feb61db08ce94d988d2c67278
Paris Kasidiaris <paris@sourcelair.com>
2018-06-14 13:19:15 +0300

    Migrate to TypeScript

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+class SplitLayout {
+  public readonly gutter: HTMLElement;
+  private isResizing: boolean = false;
+  private rect: DOMRect;
+
+  constructor(public readonly container: HTMLElement) {
+    this.gutter = this.container.querySelector('.split-pane-gutter');
+
+    this.gutter.addEventListener('mousedown', e => {
+      e.stopPropagation();
+      this.isResizing = true;
+      this.rect = <DOMRect>this.container.getBoundingClientRect();
+      this.container.addEventListener('mousemove', this.resize);
+    });
+
+    window.addEventListener('mouseup', e => {
+      if (this.isResizing) {
+        e.stopPropagation();
+        this.isResizing = false;
+        this.container.removeEventListener('mousemove', this.resize);
+      }
+    });
+  }
+
+  private resize = e => {
+    const min = 100;
+    const max = 700;
+    const pos = e.clientX - this.rect.left;
+
+    if (pos < min || pos > max) {
+      return;
+    }
+
+    const gridTemplateColumns = `${pos}px 5px 5px auto`;
+    this.container.style.gridTemplateColumns = gridTemplateColumns;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  new SplitLayout(document.querySelector('#le-layout'));
+});
