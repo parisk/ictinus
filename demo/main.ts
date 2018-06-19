561feb4e07793e430224e184b278eebf36576833
Paris Kasidiaris <paris@sourcelair.com>
2018-06-19 12:23:56 +0300

    Add node-sass into the game and simplify demo

--- demo/main.ts
+++
@@ -1,9 +0,0 @@
-///<amd-module name='dist/main'/>
-
-import { SplitLayout } from '../src/split';
-
-
-console.debug('a');
-
-new SplitLayout(document.querySelector('#horizontal-example'), 'horizontal');
-new SplitLayout(document.querySelector('#vertical-example'), 'vertical', 20);
